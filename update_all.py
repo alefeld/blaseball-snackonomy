b473b8d5d0bdd2993bb7e85adf221f99f861700a
Anthony <alefeld@alumni.nd.edu>
2021-06-07 16:42:09 -0400

    Only run updates based on phase -Phase 0 (Rest) and Phase 13 (Election) are time when no updates are expect, so save some cycles by aborting

--- update_all.py
+++ update_all.py
@@ -8,14 +8,16 @@ import datetime
 spreadsheet_id = '1_p6jsPxMvO0nGE-fiqGfilu-dxeUc994k2zwAGNVNr0'
 
 def update_all():
-    print("Start Timestamp: {:%Y-%b-%d %H:%M:%S}".format(datetime.datetime.now()))
-
     statsheets.update()
     hitterstats.update(spreadsheet_id)
     weathersnacks.update(spreadsheet_id)
     tomorrowpitchers.update(spreadsheet_id)
 
-    print("End Timestamp: {:%Y-%b-%d %H:%M:%S}".format(datetime.datetime.now()))
-
 if __name__ == "__main__":
-    update_all()
+    print("Start Timestamp: {:%Y-%b-%d %H:%M:%S}".format(datetime.datetime.now()))
+    sim = bb.get_simulation_data()
+    if sim['phase'] in [0,13]:
+        print("Siesta! Go to sleep!")
+    else:
+        update_all()
+    print("End Timestamp: {:%Y-%b-%d %H:%M:%S}".format(datetime.datetime.now()))

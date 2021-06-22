023014c80a641e1a86915371388d0f01e772b09a
Anthony Lefeld <alefeld@Anthonys-MacBook-Pro.local>
2021-06-22 14:40:58 -0400

    Added support to run after earlsiesta/latesiesta -Things seem to crash whenever the site goes down for seista/election updates, so need to fix that

--- snack_waiter.py
+++ snack_waiter.py
@@ -14,6 +14,7 @@ stream = SSEClient('http://blaseball.com/events/streamData', retry=1000)
 phase_last = -1
 season_last = -1
 day_last = -1
+siesta_processed = False
 for message in stream:
     # At seemingly fixed intervals, the stream sends an empty message
     if not str(message):
@@ -32,13 +33,17 @@ for message in stream:
     # If this day hasn't been processed, run if games are finished. Also run if we switch phases
     if day != day_last or season != season_last or phase != phase_last:
         all_finished = all([schedule['finalized'] for schedule in schedules])
-        if all_finished:
+        if all_finished or (day in [28,73] and not siesta_processed):
             logging.info("Waiter is now running snack errands.")
             logging.info("Start Timestamp: {:%Y-%b-%d %H:%M:%S}".format(datetime.datetime.now()))
             update_all.update_all()
             season_last = season
             day_last = day
             phase_last = phase
+            if day in [28,73]:
+                siesta_processed = True
+            if day in [29,74]:
+                siesta_processed = False
             logging.info("End Timestamp: {:%Y-%b-%d %H:%M:%S}".format(datetime.datetime.now()))
     else:
         pass
